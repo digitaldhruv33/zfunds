d488483d1276c675043bd558a10a6907b824bff8
digitaldhruv33 <digitaldhruv33@gmail.com>
2024-02-16 19:46:53 +0530

    test

--- app/utils/jwt.utils.ts
+++ app/utils/jwt.utils.ts
@@ -4,6 +4,7 @@ import Users from '../modules/users/user.model';
 export const generateAccessToken = async (user: Users) => {
 	const accessTokenSecret: string = process.env.JWT_ACCESS_TOKEN_SECRET!;
 	console.log('accessTokenSecret')
+	console.log('accessTokenSecret')
 	const { id, mobile_number, role } = user;
 	return new Promise((res, rej) => {
 		jwt.sign(
